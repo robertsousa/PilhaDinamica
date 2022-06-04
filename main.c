272cf700db80115ac5432aa63a56955141d00c2f
Roberto Sousa <robert6257@gmail.com>
2022-06-04 12:06:08 -0300

    Definido estrutura e prototipação

--- main.c
+++ main.c
@@ -1,5 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+
+//Definindo a estrutura
+struct no *ptr_no;
+ptr_no pilha;
+int op;
+
+//prototipação
+void menu_mostrar();
+void menu_selecionar(int op);
+void pilha_inserir(ptr_no pilha);
+void pilha_remover(ptr_no pilha);
+void pilha_mostrar(ptr_no pilha);
+
 
 int main()
 {
